08e8a677fb824032bb79e30fa522c8695d0545cc
Brandon Surpris <bsurpris2@gmail.com>
2026-01-22 22:49:45 -0800

    Minor Changes

--- Server/index.js
+++ Server/index.js
@@ -4,25 +4,6 @@ const port = 8080;
 
 app.use(express.json());
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: '👚',
-        size: 'Medium'
-    })
-});
-
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        return res.status(418).send({ message: "Logo is required" })
-    }
-
-    res.send({
-        tshirt: `👕 with your logo ${logo} and id ${id}`
-    });
-});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
